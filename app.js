e9946340fae8881f7f71112253ae0f0170747e83
Tessa Ganser <t.m.ganser@gmail.com>
2017-05-09 16:54:47 -0500

    finished in class exercise

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ var path = require( 'path' );
 // uses
 app.use( express.static( 'public' ) );
 app.use( bodyParser.urlencoded( { extended: true } ) );
+app.use( bodyParser.json());
 // routes
+var arrayOfMoose =[DRAKEY={"name":"drakey"}, MOOSY={"name":"moosy"}, CANADAY={"name":"canaday"}];
 // app.use( '/', index );
 app.get( '/', function( req, res ){
   res.sendFile( path.resolve( 'public/views/index.html' ) );
@@ -19,4 +21,16 @@ var port = process.env.PORT || 3456;
 // spin up server
 app.listen( port, function() {
   console.log( 'server up on:', port );
+});
+
+app.get('/getmooses', function(req, res){
+  console.log('in the getmooses route:');
+  res.send(arrayOfMoose);
+});
+
+app.post('/addmooses', function(req,res){
+  arrayOfMoose.push(req.body);
+  console.log('req.body: ', req.body );
+  console.log(arrayOfMoose );
+  res.sendStatus(201);
 });
--- public/scripts/ngbasics.js
+++ public/scripts/ngbasics.js
@@ -2,13 +2,25 @@ console.log("test");
 
 var ourApp = angular.module('ourApp', []);
 
-ourApp.controller('Controlla', function(){
-  console.log('drake');
-
+ourApp.controller('Controlla', function($http){
   var vm = this;
+  // vm.moose = []; /refactoring out to the server
 
-  vm.moose = [];
+  // GET THE MOOSES
+  // connect to server and bring back all the mooses there
+  vm.getMooses = function(){
+    $http({
+      method: 'GET',
+      url: '/getmooses'
+    }).then(function success(response) {
+      console.log("we got this from the server: ", response.data);
+      vm.drakesmooses = response.data;
+      });
+  };
 
+  // vm.getMooses();
+
+//function to add moose to the array (we will refactor this to the server) TODO
   vm.addMoose = function(){
     console.log("in add moose function ng-click");
 
@@ -17,17 +29,24 @@ ourApp.controller('Controlla', function(){
       description: vm.descriptionIn,
       hobby: vm.hobbyIn
     };
-
     console.log('adding moose : ', newMoose);
 
-    vm.moose.push(newMoose);
-    console.log('mooses: ', vm.moose);
-
+    // vm.moose.push(newMoose);
+    // console.log('mooses: ', vm.moose);
     vm.nameIn = "";
     vm.descriptionIn = "";
     vm.hobbyIn = "";
 
-  };
+    //tryign to send this moose to the database but he DOESNT WANT TO GO
+    //he changed his mind.
+    $http({
+      method: 'POST',
+      url: '/addmooses',
+      data: newMoose,
+      }).then(function success(response) {
+      console.log("we got this from the server: ", response);
+      vm.getMooses();
+      });
+  };//end addMoose
 
-
-});
+  });//end controlla
